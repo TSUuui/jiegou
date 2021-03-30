d8c64968633167609f839a86c270175feda6e46a
linqiyuan <466214222@qq.com>
2021-03-30 17:43:02 +0800

    完成登录、注册

---
+++ assets/commonJs.js
@@ -0,0 +1,12 @@
+//首页
+function backHome() {
+	window.location = window.location.origin + '/jiegou/index/index.html'
+}
+//跳转注册页面
+function toRegister() {
+	window.location = window.location.origin + '/jiegou/Register/Register.html'
+}
+//跳转登录页面
+function toLogin() {
+	window.location = window.location.origin + '/jiegou/Login/Login.html'
+}
